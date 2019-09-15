b2c93d1324c9e4c13c3b74669f55199d157d6cc9
doblotoon <victorhugonass@gmail.com>
2019-09-15 20:52:45 -0300

    eu sou o  Deus do array multidimensional nãi indexado mano, agora eu posso filtrar os dados por uma pergunta específica

---
+++ novo.php
@@ -0,0 +1,283 @@
+<?php
+
+$json = file_get_contents('dados.json');
+$dados = json_decode($json, true);
+
+
+echo "<pre>";
+//print_r($dados);
+echo "</pre>";
+foreach ($dados as $key => $value) {
+
+    if ($value['curso'] == 'info') { ### escolhendo uma informação específica
+        $dados_especificos[] = $value;
+    }
+    /*foreach ($value as $key => $value) {
+        if ($value == 'info') {
+            $info[] = $key; 
+        }
+    }*/
+}
+/*echo "<pre>";
+print_r($dados_especificos);
+echo "<pre>";
+exit;*/
+
+foreach ($dados_especificos as $key => $value) {// tem uma variável pra cada pergunta do json
+    foreach ($value as $key => $value) {
+        /*echo "<pre>";
+        print("{$key} => {$value}");
+        echo "<pre>";*/
+
+        switch ($key) {
+            case 'curso':
+                $curso[] = $value;
+                break;
+            
+            case 'ano':
+                $ano[] = $value;
+                break;
+            
+            case 'cor':
+                $cor[] = $value;
+                break;
+            
+            case 'idade':
+                $idade[] = $value;
+                break;
+            
+            case 'sexo':
+                $sexo[] = $value;
+                break;
+            
+            case 'orientacao':
+                $orientacao[] = $value;
+                break;
+            
+            case 'religiao':
+                $religiao[] = $value;
+                break;
+
+            case 'moradia':
+                $moradia[] = $value;
+                break;
+
+            case 'trabalho':
+                $trabalho[] = $value;
+                break;
+            
+            case 'fundamental':
+                $fundamental[] = $value;
+                break;
+
+            case 'escolhaIF':
+                $escolhaIF[] = $value;
+                break;
+
+            case 'ensinoSuperior':
+                $ensinoSuperior[] = $value;
+                break;
+
+            case 'streaming':
+                $streaming[] = $value;
+                break;
+
+            case 'politica':
+                $politica[] = $value;
+                break;
+
+            case 'alimentacao':
+                $alimentacao[] = $value;
+                break;
+
+            case 'atividadeFisica':
+                $atividadeFisica[] = $value;
+                break;
+
+            case 'drogas':
+                $drogas[] = $value;
+                break;
+
+            case 'renda':
+                $renda[] = $value;
+                break;
+
+            case 'divorcio':
+                $divorcio[] = $value;
+                break;
+
+            case 'irmao':
+                $irmao[] = $value;
+                break;
+
+            case 'cansacoAula':
+                $cansacoAula[] = $value;
+                break;
+
+            case 'pressao':
+                $pressao[] = $value;
+                break;
+
+            case 'tempoEstudo':
+                $tempoEstudo[] = $value;
+                break;
+
+            case 'relacionamentosIF':
+                $relacionamentosIF[] = $value;
+                break;
+
+            case 'estadoEspirito':
+                $estadoEspirito[] = $value;
+                break;
+
+            case 'tempoParticular':
+                $tempoParticular[] = $value;
+                break;
+
+            case 'prodCultural':
+                $prodCultural[] = $value;
+                break;
+
+            case 'suicidio':
+                $suicidio[] = $value;
+                break;
+            default:
+                //algo deu muito errado;
+                break;
+        }
+        //$curso[$key] .=$value ;
+    }
+}
+function getJSON($arquivo){
+    $json = file_get_contents("{$arquivo}");
+    $dados = json_decode($json, true);
+    return $dados;
+}
+
+function variavel($dados, $chaveJson){// n tá funfando
+    foreach ($dados as $key => $value) {
+        foreach ($value as $chave => $valor) {
+            if ($chaveJson == $chave) {
+                $dados[] = $valor;
+            }
+        }
+    }
+    return $dados;
+}
+
+
+/**
+ * Retorna o valor que mais aparece no array (moda estatistica)
+ * @param array $a Array de valores
+ * @param int $quantidade Quantidade de vezes que a moda foi observada
+ * @return array Valores mais observados no array
+ */
+function moda(array $a, &$quantidade = 0) { 
+//funcao que dá a moda definitiva
+//peguei pronta de um site
+    $moda = array();
+    if (empty($a)) {
+        return $moda;
+    }
+
+    // Calcular quantidade de ocorrencias de cada valor
+    $ocorrencias = array();
+    foreach ($a as $valor) {
+        $valor_str = var_export($valor, true);
+        if (!isset($ocorrencias[$valor_str])) {
+            $ocorrencias[$valor_str] = array(
+                'valor' => $valor,
+                'ocorrencias' => 0
+            );
+        }
+        $ocorrencias[$valor_str]['ocorrencias'] += 1;
+    }
+
+    // Determinar maior ocorrencia
+    $quantidade = null;
+    foreach ($ocorrencias as $item) {
+        if ($quantidade === null || $item['ocorrencias'] >= $quantidade) {
+            $quantidade = $item['ocorrencias'];
+        }
+    }
+
+    // Obter valores com a maior ocorrencia
+    foreach ($ocorrencias as $item) {
+        if ($item['ocorrencias'] == $quantidade) {
+            $moda[] = $item['valor'];
+        }
+    }
+    return $moda;
+}
+function calcModa($pergunta){
+// funcao que dá os dados da moda
+    $moda = array_count_values($pergunta);
+    return $moda;
+}
+
+function porcentagem($moda){
+    foreach ($moda as $key => $value) {
+        $dados[$key] = number_format((100*$value)/246, 1, '.','');
+    }
+    return $dados;
+//pega a moda de cada resposta e já calcula as suas porcentagens
+}
+//print_r(porcentagem(calcModa($religiao)));
+/*
+$a = porcentagem(calcModa($curso)); 
+                foreach ($a as $key => $value) {
+                    print("{$value}%, ");
+                }*/
+//print_r(calcModa($ano));
+//print_r(array_count_values($curso)); //moda
+?>
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+
+            <?php
+                echo "['pergunta', 'porcentagem'],";
+                $a = calcModa($cor);
+                foreach ($a as $key => $value) {
+                    echo "['{$key}', {$value}],";
+                }
+            ?>
+
+
+          /*['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]*/
+        ]);
+
+        var options = {
+          title: 'posicionamento político (1 - esquerda | 2 - centro | 3 - direita)',
+          pieHole: 0.6,
+          height: 600,
+          width: 900
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+
+         google.visualization.events.addListener(chart, 'ready', function () {
+        chart_div.innerHTML = '<img src="' + chart.getImageURI() + '">';
+        console.log(chart_div.innerHTML);
+      });
+
+        chart.draw(data, options);
+
+
+      }
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
